8db4c272050f1ab8e8c5135ca19e23fc3703679b
kmuralidharan91 <kmuralidharan91@gmail.com>
2015-07-15 20:31:44 +0530

    Home, Splash Activity Added

---
+++ app/src/main/java/ashok/com/twosides/HomeActivity.java
@@ -0,0 +1,9 @@
+package ashok.com.twosides;
+
+import android.app.Activity;
+
+/**
+ * Created by MuraliKDharan on 15/07/15.
+ */
+public class HomeActivity extends Activity {
+}
---
+++ app/src/main/java/ashok/com/twosides/SplashActivity.java
@@ -0,0 +1,48 @@
+package ashok.com.twosides;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.os.Handler;
+
+/**
+ * Created by MuraliKDharan on 15/07/15.
+ */
+public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_TIMER = 5000;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_TIMER);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
